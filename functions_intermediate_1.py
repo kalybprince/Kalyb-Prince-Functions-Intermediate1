8519c0bb6ad7d11208b4cfd74cddb106239f02aa
Kalyb Prince <kalybprince91@gmail.com>
2021-08-02 14:21:24 -1000

    init functions_intermediate_1.py

---
+++ functions_intermediate_1.py
@@ -0,0 +1,78 @@
+"""
+PROBLEM 1
+"""
+
+# x = [ [5,2,3], [10,8,9] ] 
+
+# students = [
+#     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#     {'first_name' : 'John', 'last_name' : 'Rosales'}
+# ]
+
+# sports_directory = {
+#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+# }
+
+# z = [ {'x': 10, 'y': 20} ]
+
+# 1
+
+# x[1][0] += 5
+
+# 2
+
+# students[0]['last_name'] = 'Bryant'
+
+# 3
+
+# sports_directory['soccer'][0] = 'Andres'
+
+# 4 
+
+# z[0]['y'] = 20
+
+
+"""
+PROBLEM 2
+"""
+
+# students = [
+#     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#     {'first_name' : 'John', 'last_name' : 'Rosales'},
+#     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+# ]
+
+# def iterateDictionary(some_list):
+#     for i in some_list:
+#         print(f"first_name - {i['first_name']}, last_name - {i['last_name']}")
+
+# iterateDictionary(students)
+
+
+"""
+PROBLEM 3
+"""
+
+# def iterateDictionary2(key_name, some_list):
+#     for i in some_list:
+#         print(f'{i[key_name]}')
+
+
+"""
+PROBLEM 4
+"""
+
+# dojo = {
+#     'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+#     'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+# }
+
+# def printInfo(some_dict):
+#     for i, j in some_dict.items():
+#         print(f"{len(dojo)} {i.upper()}")
+#         for k in j:
+#             print(k)
+
+# printInfo(dojo)
